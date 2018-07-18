4ca8ff0dc3308e18e43eace73b2c69d2e1210382
Aritzherrero4 <aritzherrero4@gmail.com>
2018-07-18 05:48:38 -0700

    build_script.sh: Redo build_kernel() to prevent permission conflicts

--- build_script.sh
+++ build_script.sh
@@ -18,12 +18,14 @@ function echo() {
 
 function die() {
     echo
-    echo -e "${RED}${1}${RST}"
+    echo -e "${RED}${1}"
     [[ ${2} = "-h" ]] && ${0} -h
+    echo  "${RST}"
     exit 1
 }
 
 function sign_kernel() {
+echo "sign kernel"
 cd
 java -jar "zipsigner-2.1.jar" \
               "flash-AH.zip" \
@@ -31,51 +33,49 @@ java -jar "zipsigner-2.1.jar" \
 }
 
 function zip_kernel() {
+echo "zip kernel"
 cd 
 cd ${AK2}
 zip -r9 ${kernel_name}.zip * -x README.md ${kernel_name}.zip
 cd
-mv ${AK2}/${kernel_name}.zip ${kout}
+mv ${AK2}/flash-AH.zip /home/aritzherrero4/
 }
 
-# Clean up
+# Clean up and prepare anykernel2 for 5/5T
 function clean_up() {
     # Clean AnyKernel2 folder if building a zip
         cd 
         cd "${AK2}" || die "AnyKernel2 source is missing!"
         git checkout  ${AK2_branch}
         git clean -fxd 
- 
-
-    # clean kernel
-    cd 
-    cd "${k}" || die "Kernel source is missing!"
-    git checkout ${kernel_branch}
-    git clean -fxd
-       
+	mkdir kernels
+	cd kernels
+	for OS in ${OS_LIST}; do
+		mkdir ${OS}
+	done
+     
 }
 
 function build_kernel() {
-   cd 
-   cd ${k}
-   #make 2 compilations, for OOS and custom
-   make O=out/oos ARCH=arm64 flash-oos_defconfig
 
-   make -j$(nproc --all) O=out/oos \
+OS_LIST="OOS Custom"
+
+for OS in ${OS_LIST}; do
+	cd 
+        cd "${k}" || die "Kernel source is missing!"
+	git checkout ${kernel_branch}
+   	git clean -fxd
+        
+        make O=out/${OS} ARCH=arm64 flash-${OS,,}_defconfig
+
+   	make -j$(nproc --all) O=out/${OS} \
                       ARCH=arm64 \
                       CC="${clang}/bin/clang" \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE="${gcc}/bin/aarch64-linux-gnu-"
- 
+        mv ${k}/out/${OS}${kdir} ${AK2}/kernels/${OS}
+done
 
-   make O=out/custom ARCH=arm64 flash-custom_defconfig
-
-   make -j$(nproc --all) O=out/custom \
-                      ARCH=arm64 \
-                      CC="${clang}/bin/clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${gcc}/bin/aarch64-linux-gnu-"
- 
 }
 function end_info() {
     echo "SCRIPT FINISHED!"
@@ -91,19 +91,6 @@ function end_info() {
     exit
 }
 
-function move_img() {
-kdir=/arch/arm64/boot/Image.gz-dtb
-cd
-cd ${AK2}
-mkdir kernels
-cd kernels
-mkdir oos
-mkdir custom
-cd
-mv ${k}/out/oos${kdir} ${AK2}/kernels/oos
-mv ${k}/out/custom${kdir} ${AK2}/kernels/custom
-
-}
 ################
 #  VARIABLES   #
 ################
@@ -111,11 +98,11 @@ mv ${k}/out/custom${kdir} ${AK2}/kernels/custom
 #AnyKernel2folder
 AK2=/home/aritzherrero4/AK2
 #Anykernel2 branch
-AK2_branch=op5-flash-8.x
+AK2_branch=op5-flash-8.x-treble
 #kernel folder
 k=/home/aritzherrero4/flash
 #kernel-branch
-kernel_branch=8.1.0-unified
+kernel_branch=8.1.0-unified-treble
 #compiler folder
 gcc=/home/aritzherrero4/gcc
 #clang folder
@@ -123,11 +110,13 @@ clang=/home/aritzherrero4/clang
 #kernel name
 kernel_name=flash-AH
 #compiled, zip and signed out folder
-Kout=/home/aritzherrero4/fkz
-
+kout=/home/aritzherrero4
+#kernel image directory
+kdir=/arch/arm64/boot/Image.gz-dtb
+#OS_List
+OS_LIST="OOS Custom"
 
 ################
-#              #
 # SCRIPT START #
 ################
 
@@ -138,18 +127,13 @@ clear
 echo "${RED}"
 echo
 echo "================================================================================================"
-echo
-echo
 echo "  ___________________________________  __   ______ _______________________   ________________   "
 echo "  ___  ____/__  /___    |_  ___/__  / / /   ___  //_/__  ____/__  __ \__  | / /__  ____/__  /   "
 echo "  __  /_   __  / __  /| |____ \__  /_/ /    __  ,<  __  __/  __  /_/ /_   |/ /__  __/  __  /    "
 echo "  _  __/   _  /___  ___ |___/ /_  __  /     _  /| | _  /___  _  _, _/_  /|  / _  /___  _  /___  "
 echo "  /_/      /_____/_/  |_/____/ /_/ /_/      /_/ |_| /_____/  /_/ |_| /_/ |_/  /_____/  /_____/  "
-echo
-echo
-echo
 echo "================================================================================================"
-echo
+echo  "${RST}"
 
 
 #################
@@ -159,12 +143,9 @@ echo
 
 # Clean up the source
 clean_up
-
 # Build kernel
 build_kernel
-
 # After build tasks
-move_img
 zip_kernel
 sign_kernel
 # Print file info and time
