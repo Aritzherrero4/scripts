cfd0b363aa4db33cbe11af97da6d96314e7a182a
Aritz Herrero <aritzherrero4@gmail.com>
2019-10-08 23:58:19 +0200

    Add a new script to build Amethyst kernel

---
+++ buildkernel.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+cd ..
+
+# Set Kernel Info
+export VERA="amethyst"
+export VERB="$(date +%Y%m%d)"
+VERSION="${VERA}-${VERB}"
+
+# Export User and Host 
+export KBUILD_BUILD_USER=aritzherrero4
+export KBUILD_BUILD_HOST=amethyst-build
+
+#Set compiler
+export COMPILER=GCC
+export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-elf-"
+export CROSS_COMPILE_ARM32="$(pwd)/gcc32/bin/arm-eabi-"
+export STRIP="$(pwd)/gcc/bin/aarch64-elf-strip"
+export ARCH=arm64 && export SUBARCH=arm64
+
+# Export versions
+export LOCALVERSION=`echo -${VERSION}`
+export ZIPNAME="${VERSION}.zip"
+#compilation
+cd kernel8998
+START=$(date +"%s")
+make O=out ARCH=arm64 amethyst_defconfig
+make -j$(nproc --all)  O=out ARCH=arm64
+
+END=$(date +"%s")
+DIFF=$((END - START))
+cd ..
+
+cp $(pwd)/kernel8998/out/arch/arm64/boot/Image.gz-dtb $(pwd)/anykernel
+
+cd anykernel
+#clear previous builds
+rm *.zip
+zip -r9 ${ZIPNAME} * -x README.md ${ZIPNAME}
+CHECKER=$(ls -l ${ZIPNAME} | awk '{print $5}')
+rm *.gz-dtb
+
+
+if (($((CHECKER / 1048576)) > 5)); then
+	echo "Build completed in $DIFF seconds"
+	else
+	echo "ERROR"
+	exit 1;
+fi
